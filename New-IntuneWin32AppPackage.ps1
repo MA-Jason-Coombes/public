5e2d82b19c0c6357019c7f8bd1f949d28d3b5dbd
MA-Jason-Coombes <143633191+MA-Jason-Coombes@users.noreply.github.com>
2023-11-05 02:55:03 +0000

    Update New-IntuneWin32AppPackage.ps1

--- New-IntuneWin32AppPackage.ps1
+++ New-IntuneWin32AppPackage.ps1
@@ -101,7 +101,7 @@ function New-IntuneWin32AppPackage {
                         if ($ProcessPackage -eq $true) {
                             # Invoke IntuneWinAppUtil.exe with parameter inputs
                             Write-Verbose -Message "Invoking IntuneWinAppUtil.exe to initialize packaging process"
-                            $PackageInvocation = Start-Process -FilePath $IntuneWinAppUtilPath -ArgumentList "-c `""$($SourceFolder)`"", "-s `""$($SetupFile)`"", "-o `""$($OutPutFolder)`"", "-q" -PassThru -wait
+                            $PackageInvocation = Start-Process -FilePath $IntuneWinAppUtilPath -ArgumentList "-c `"$($SourceFolder)`"", "-s `""$($SetupFile)`"", "-o `""$($OutPutFolder)`"", "-q" -PassThru -wait
                             if ($PackageInvocation.ExitCode -eq 0) {
                                 Write-Verbose -Message "IntuneWinAppUtil.exe packaging process completed with exit code $($PackageInvocation.ExitCode)"
 
